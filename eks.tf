f164908f3b35488ffb01b06d24b8bdb514be6604
hardik2302 <hardikagrawal@gmail.com>
2025-03-14 17:36:31 +0000

    Add Node Group

--- eks.tf
+++ eks.tf
@@ -6,3 +6,16 @@ resource "aws_eks_cluster" "eks_cluster" {
     subnet_ids = [aws_subnet.eks_subnet_1.id, aws_subnet.eks_subnet_2.id]
   }
 }
+
+resource "aws_eks_node_group" "eks_nodes" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "eks-node-group"
+  node_role_arn   = aws_iam_role.eks_worker_role.arn
+  subnet_ids      = [aws_subnet.eks_subnet_1.id, aws_subnet.eks_subnet_2.id]
+  instance_types  = ["t3.medium"]
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+}
